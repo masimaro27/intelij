5599a91264c32f340b402d89a06f1a6e775c5261
minhyuk <minhyuki89@gmail.com>
2020-08-05 11:17:59 +0900

    intelij playground init

---
+++ JAVA/src/playground/minhyuk/Main.java
@@ -0,0 +1,29 @@
+package playground.minhyuk;
+
+public class Main {
+
+    String stylus;
+    test
+
+
+    public static void main(String[] args) {
+	    System.out.println("HELLO INTELIJ");
+
+
+
+
+    }
+
+    public int getTest(){
+        return TEST;
+    }
+    
+    public String getStylus() {
+        return setStylus();
+    }
+
+    private String setStylus() {
+        return stylus;
+    }
+
+}
---
+++ JAVA/src/playground/minhyuk/move/Main2.java
@@ -0,0 +1,16 @@
+package playground.minhyuk.move;
+
+public class Main2 {
+
+
+    String test;
+    String test2;
+
+
+    public Main2() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("HELLO INTELIJ");
+    }
+}
