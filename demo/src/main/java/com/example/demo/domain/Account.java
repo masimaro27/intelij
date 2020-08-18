39ad6819f2aeb91101172619bf96e47e38998d70
minhyuk <minhyuki89@gmail.com>
2020-08-18 16:56:03 +0900

    [JPA] sample project 틀

---
+++ demo/src/main/java/com/example/demo/domain/Account.java
@@ -0,0 +1,20 @@
+package com.example.demo.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ACCOUNT")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    @Column(name = "EMAIL")
+    private String email;
+    private String password;
+    private String username;
+
+
+
+
+}
---
+++ demo/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,12 @@
+drop table account if exists;
+drop sequence if exists hibernate_sequence;
+
+create table account (
+    id bigint not null auto_increment,
+    email varchar(255),
+    password varchar(255),
+    username varchar(255),
+    primary key (id)
+);
+
+insert into ACCOUNT (EMAIL, PASSWORD, USERNAME) values ('test@test.com', 'password', 'username');
