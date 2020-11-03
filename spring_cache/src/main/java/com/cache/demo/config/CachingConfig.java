e6e6a1033eb9af0e084be547fe2bc41d5e7743c9
minhyuk <minhyuki89@gmail.com>
2020-11-03 09:46:33 +0900

    spring_cache 샘플

---
+++ spring_cache/src/main/java/com/cache/demo/config/CachingConfig.java
@@ -0,0 +1,27 @@
+package com.cache.demo.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.PermissionEvaluator;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableCaching
+@ComponentScan("com.cache.demo")
+public class CachingConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        final SimpleCacheManager cacheManager = new SimpleCacheManager();
+        cacheManager.setCaches(Arrays.asList(new ConcurrentMapCache("directory"), new ConcurrentMapCache("addresses")));
+        return cacheManager;
+    }
+
+}
